73bd03675eb5b65bf533cccbb96d43cee70b5dee
Omar Yosrii <omarr.yosrii@gmail.com>
2026-05-12 20:42:00 +0300

    Initial spring boot setup

---
+++ src/main/java/com/rwms/RwmsApplication.java
@@ -0,0 +1,13 @@
+package com.rwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RwmsApplication.class, args);
+	}
+
+}
