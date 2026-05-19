74505cb7abe59ec3e7d3c7d01a8f2aff29a125b2
Omar Yosrii <omarr.yosrii@gmail.com>
2026-05-19 19:52:34 +0300

    implement user management module with service interface, DTOs, repository, and mapper skeletons

---
+++ src/main/java/com/rwms/user/dto/UpdateUserRequest.java
@@ -0,0 +1,18 @@
+package com.rwms.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+}
